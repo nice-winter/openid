bb28f7d7741b94e28ce19bc3291aef5b94dff4cb
hhui64 <907322015@qq.com>
2025-04-28 19:23:54 +0800

    feat: add navigation menu and header component to App.vue

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -1,8 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const route = useRoute()
+
+const items = computed<NavigationMenuItem[]>(() => [
+  {
+    label: 'Docs',
+    to: '/getting-started',
+    active: route.path.startsWith('/getting-started')
+  },
+  {
+    label: 'Components',
+    to: '/components',
+    active: route.path.startsWith('/components')
+  },
+  {
+    label: 'Releases',
+    to: 'https://github.com/nuxt/ui/releases',
+    target: '_blank'
+  }
+])
+</script>
 
 <template>
   <UApp>
-    <header>
+    <!-- <header>
       <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
       <div class="wrapper">
@@ -13,7 +35,30 @@
           <RouterLink to="/about">About</RouterLink>
         </nav>
       </div>
-    </header>
+    </header> -->
+
+    <UHeader>
+      <template #title>
+        <VLogo class="h-6 w-auto" />
+      </template>
+
+      <UNavigationMenu :items="items" />
+
+      <template #right>
+        <UColorModeButton />
+
+        <UTooltip text="Open on GitHub" :kbds="['meta', 'G']">
+          <UButton
+            color="neutral"
+            variant="ghost"
+            to="https://github.com/nuxt/ui"
+            target="_blank"
+            icon="i-lucide-github"
+            aria-label="GitHub"
+          />
+        </UTooltip>
+      </template>
+    </UHeader>
 
     <RouterView />
   </UApp>
